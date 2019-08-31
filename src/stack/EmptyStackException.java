c4fcb98e1ef2a3f0b31ff7578334261de55c1be5
jaykybert <kybertjay@gmail.com>
2019-08-31 17:07:05 +0100

    Replaced IndexOutOfBounds with custom exceptions.

---
+++ src/stack/EmptyStackException.java
@@ -0,0 +1,8 @@
+package stack;
+
+public class EmptyStackException extends Exception {
+	
+	public String toString() {
+		return "Stack is empty.";
+	}
+}
---
+++ src/stack/FullStackException.java
@@ -0,0 +1,8 @@
+package stack;
+
+public class FullStackException extends Exception {
+	
+	public String toString() {
+		return "Stack is full.";
+	}
+}
